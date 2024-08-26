90309d880c613c2bf6f8761e745818ec3d374ab9
Richard Szilagyi <szprichard@proton.me>
2024-08-27 00:44:02 +0100

    chore(i18n): Set default locale to 'hu' in i18n configuration.

--- i18n.config.ts
+++ i18n.config.ts
@@ -1,27 +1,27 @@
-import ara from './locales/ara.js'
-import de from './locales/de.js'
-import en from './locales/en.js'
-import fr from './locales/fr.js'
-import hu from './locales/hu.js'
-import it from './locales/it.js'
-import ptBr from './locales/pt-br.js'
-import ru from './locales/ru.js'
-import sp from './locales/sp.js'
-import tr from './locales/tr.js'
+import ara from "./locales/ara.js";
+import de from "./locales/de.js";
+import en from "./locales/en.js";
+import fr from "./locales/fr.js";
+import hu from "./locales/hu.js";
+import it from "./locales/it.js";
+import ptBr from "./locales/pt-br.js";
+import ru from "./locales/ru.js";
+import sp from "./locales/sp.js";
+import tr from "./locales/tr.js";
 
 export default defineI18nConfig(() => ({
-  legacy: false,
-  locale: 'en',
-  messages: {
-    ara,
-    de,
-    en,
-    fr,
-    hu,
-    it,
-    ptBr,
-    ru,
-    sp,
-    tr
-  }
-}))
+	legacy: false,
+	locale: "hu",
+	messages: {
+		ara,
+		de,
+		en,
+		fr,
+		hu,
+		it,
+		ptBr,
+		ru,
+		sp,
+		tr,
+	},
+}));
